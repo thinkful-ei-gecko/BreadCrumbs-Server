7179348ea97e48ebbfe17caace365413628cce33
Shawn Collette <shawn.collette@gmail.com>
2019-12-09 11:56:05 -0700

    cancel vote feature

--- src/vote/vote-router.js
+++ src/vote/vote-router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { requireAuth } = require('../middleware/jwt-auth');
 const VoteService = require('./vote-service');
-const ArticleService = require('../article/article-service');
 
 const voteRouter = express.Router();
 const jsonParser = express.json();
@@ -21,12 +20,6 @@ voteRouter
     let newVoteType;
     let vote_id;
 
-    // console.log(oldVoteType)
-    // console.log(vote[0].vote_type)
-    // console.log(newVoteCount)
-    // console.log(vote[0].vote_count)
-    // console.log(newVoteType)
-    // console.log(req.body.vote_type)
     VoteService.insertArticleVote(db,article_id,user_id)
       .then(()=>VoteService.getVoteData(db, user_id, article_id))
       .then(vote => {
@@ -36,15 +29,6 @@ voteRouter
         newVoteType = req.body.vote_type;
         vote_id = vote[0].id;
   
-
-        // console.log(oldVoteType)
-        // console.log(vote[0].vote_type)
-        // console.log(newVoteCount)
-        // console.log(vote[0].vote_count)
-        // console.log(newVoteType)
-        // console.log(req.body.vote_type)
-  
-
         if(oldVoteType === null) {
           if(newVoteType === true) {
             newVoteCount++;
@@ -58,7 +42,8 @@ voteRouter
             newVoteCount=newVoteCount-2;
           }
           if(newVoteType === true) {
-            return res.status(404).json({error: {message: 'User already voted'}});
+            newvotecount++;
+            // return res.status(404).json({error: {message: 'User already voted'}});
           }
         }
         if(oldVoteType === false) {
@@ -66,71 +51,25 @@ voteRouter
             newVoteCount=newVoteCount+2;
           }
           if(newVoteType === false) {
-            return res.status(404).json({error: {message: 'User already voted'}});
+            newvotecount--;
+            // return res.status(404).json({error: {message: 'User already voted'}});
           }
         }  
-
-        // return vote;
         
-        console.log('oldVoteType',oldVoteType)
-        console.log('newVoteCount',newVoteCount)
-        console.log('newVoteType',newVoteType)
+        // console.log('oldVoteType',oldVoteType)
+        // console.log('newVoteCount',newVoteCount)
+        // console.log('newVoteType',newVoteType)
 
         VoteService.updateVoteType(db, vote_id, newVoteType)
-        VoteService.updateVoteCount(db, article_id, newVoteCount)
-      
-        
+        VoteService.updateVoteCount(db, article_id, newVoteCount)      
       })
 
-
-      // .then(vote => {
-      //   VoteService.updateVoteType(db, vote_id, newVoteType);
-      //   // return vote;
-      // })
-      // .then(vote => {
-      //   VoteService.updateVoteCount(db, article_id, newVoteCount);
-      //   // return vote;
-      // })
       .then(() => 
         VoteService.getVoteData(db, user_id, article_id))
       .then(newVote=>console.log('++++++',newVote))
-   
-    
-
-
-    // console.log(oldVoteType)
-    // console.log(vote[0].vote_type)
-    // console.log(newVoteCount)
-    // console.log(vote[0].vote_count)
-    // console.log(newVoteType)
-    // console.log(req.body.vote_type)
-
-    
-
+  
 
       .catch(next);
   });
-
-
-voteRouter
-  .all(requireAuth)
-  .route('/test')
-  .patch(jsonParser, (req, res, next) => {
-  //const { user_id, article_id, vote_type } = req.body
-    const db = req.app.get('db');
-    let user_id = 'a2424f48-132a-435c-8488-d1fc00d7afef';
-    let article_id = 'a1385155-5a02-4f8a-bfdc-e3a5379dbea0';
-    let newVoteCount = 999;
-    VoteService.updateVoteCount(db, user_id, article_id, newVoteCount)
-      .then(update => {
-        res.status(200).json(update);
-      });
-    console.log(user_id);
-    console.log(article_id);
-    console.log(newVoteCount);
-    return; 
-
-  });
-
 
 module.exports = voteRouter;
