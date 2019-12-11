622c8ec7a07ba95425cc61903227402be7d0f563
Steven Bull <stevenwbulljr@gmail.com>
2019-12-11 17:10:49 +0000

    finish update router for user account updating

--- src/update/update-router.js
+++ src/update/update-router.js
@@ -1,137 +1,126 @@
-const express = require('express')
-const path = require('path')
-const xss = require('xss');
-const UpdateService = require('./update-service')
-const AuthService = require('../auth/auth-service');
-const UserService = require('../user/user-service');
+const express = require("express");
+const xss = require("xss");
+const UpdateService = require("./update-service");
+const AuthService = require("../auth/auth-service");
+const UserService = require("../user/user-service");
 const { requireAuth } = require("../middleware/jwt-auth");
 
-const updateRouter = express.Router()
-const jsonBodyParser = express.json()
-
-const serializeUser = user => ({
-  id: user.id,
-  username: user.username,
-  name: user.name,
-  password: user.password,
-  new_password: user.new_password,
-});
-
+const updateRouter = express.Router();
+const jsonBodyParser = express.json();
 
 updateRouter
-  //.use(requireAuth)
-  .route('/')
+  .use(requireAuth)
+  .route("/")
   .all(jsonBodyParser, (req, res, next) => {
     const { user_id } = req.body;
-    const db = req.app.get('db');
+    const db = req.app.get("db");
     const id = user_id;
-  
+
     UpdateService.getById(db, id)
       .then(user => {
-        if(!user) {
-          return res.status(404).json({error: {message: `User does not exist`}})
+        if (!user) {
+          return res
+            .status(404)
+            .json({ error: { message: `User does not exist` } });
         }
-        res.user = user
-        next()
+        res.user = user;
+        next();
       })
-      .catch(next)
+      .catch(next);
   })
   .get((req, res, next) => {
-    res.json(UpdateService.serializeUser(res.user))
+    res.json(UpdateService.serializeUser(res.user));
   })
   .delete((req, res, next) => {
-    UpdateService.deleteUser(req.app.get('db'), req.body.id)
+    UpdateService.deleteUser(req.app.get("db"), req.body.id)
       .then(numRowsAffected => {
-        res.status(204).end()
+        res.status(204).end();
       })
-      .catch(next)
+      .catch(next);
   })
   .patch(jsonBodyParser, async (req, res, next) => {
-    const { username, password } = req.body
+    let { name, username, password } = req.body;
 
-    if(!username){
-      console.log('im here')
-      next()
-    }
+    name = xss(name);
+    username = xss(username);
+    password = xss(password);
 
     const compareMatch = await AuthService.comparePasswords(
       password,
       res.user[0].password
-    )
-    console.log(res.user)
-    console.log(compareMatch)
-    if(!compareMatch) {
-      res.status(404).send(`Incorrect password`)
+    );
+
+    if (!compareMatch) {
+      res.status(404).send(`Incorrect password`);
     }
 
-    const updateFields = { username };
-    const numberOfValues = Object.values(updateFields).filter(Boolean).length
+    const updateFields = { name, username };
+    const numberOfValues = Object.values(updateFields).filter(Boolean).length;
     if (numberOfValues === 0)
       return res.status(400).json({
         error: {
-          message: `Request body must contain either 'name', 'password' or 'username'`
+          message: `Request body must contain either 'name' or 'username'`
         }
-      })
-    UpdateService.updateFields(
-      req.app.get('db'),
-      res.user[0].id,
-      updateFields
-    )
+      });
+
+    UpdateService.updateFields(req.app.get("db"), res.user[0].id, updateFields)
       .then(numRowsAffected => {
-        return res.status(204).end()
+        return res.status(204).end();
       })
-      .catch(next)
-  })
+      .catch(next);
+  });
 
 updateRouter
-  //.use(requireAuth)
-  .route('/password')
+  .use(requireAuth)
+  .route("/password")
   .all(jsonBodyParser, (req, res, next) => {
     const { user_id } = req.body;
-    const db = req.app.get('db');
+    const db = req.app.get("db");
     const id = user_id;
-  
+
     UpdateService.getById(db, id)
       .then(user => {
-        if(!user) {
-          return res.status(404).json({error: {message: `User does not exist`}})
+        if (!user) {
+          return res
+            .status(404)
+            .json({ error: { message: `User does not exist` } });
         }
-        res.user = user
-        next()
+        res.user = user;
+        next();
       })
-      .catch(next)
+      .catch(next);
   })
-  .get((req, res, next) => {
-    res.json(UpdateService.serializeUser(res.user))
+  .get((req, res) => {
+    res.json(UpdateService.serializeUser(res.user));
   })
   .patch(jsonBodyParser, async (req, res, next) => {
-    const { password, new_password } = req.body
+    let { password, new_password } = req.body;
 
-    if(!new_password) {
-      next()
-    }
+    password = xss(password);
+    new_password = xss(new_password);
 
     const compareMatch = await AuthService.comparePasswords(
       password,
       res.user[0].password
-    )
- 
-    if(!compareMatch) {
-      res.status(404).send(`Incorrect password`)
+    );
+
+    if (!compareMatch) {
+      res.status(404).send(`Incorrect password`);
+    }
+
+    const passwordError = UserService.validatePassword(new_password);
+
+    if (passwordError) {
+      return res.status(400).json({ error: passwordError });
     }
 
     const newPassword = await AuthService.hashPassword(new_password);
 
-    UpdateService.updatePassword(
-      req.app.get('db'),
-      res.user[0].id,
-      newPassword
-    )
+    UpdateService.updatePassword(req.app.get("db"), res.user[0].id, newPassword)
       .then(numRowsAffected => {
-        res.status(204).end()
+        res.status(204).end();
       })
-      .catch(next)
-  })
+      .catch(next);
+  });
 
-
-module.exports = updateRouter
+module.exports = updateRouter;
